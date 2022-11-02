20b2833fe36e87bbd4b1bc4f42e33692c73aa7ea
Reckhammer <joshua.rechkemmer48@gmail.com>
2022-11-01 20:28:04 -0500

    Implemented Bottom Nav Menu
    Also created empty fragments for the fragments/screens that are directly connected to the bottom nav menu

---
+++ app/src/main/java/com/example/restaurantpicker/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.restaurantpicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.restaurantpicker.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private const val TAG = "MainActivity/"
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        val view = binding.root;
+        setContentView( view );
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val homeFragment: Fragment    = HomeFragment();
+        val listFragment: Fragment    = ListListFragment();
+        val mapFragment: Fragment     = MapFragment();
+        val settingFragment: Fragment = SettingsFragment();
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.nav_home -> fragment = homeFragment
+                R.id.nav_list -> fragment = listFragment
+                R.id.nav_map -> fragment = mapFragment
+                R.id.nav_settings -> fragment = settingFragment
+            }
+            fragmentManager.beginTransaction().replace(R.id.main_frame, fragment).commit()
+            true
+        }
+
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.nav_home
+    }
+    
+}
---
+++ app/src/main/java/com/example/restaurantpicker/MapFragment.kt
@@ -0,0 +1,33 @@
+package com.example.restaurantpicker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+private const val TAG = "MapFragment/"
+
+class MapFragment : Fragment()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    companion object
+    {
+        fun newInstance() : MapFragment
+        {
+            return MapFragment();
+        }
+    }
+}
